7870f886f7e3e7a93763e4624b8ce4718448c9f6
Sean Chen <seanchen11235@gmail.com>
2021-05-11 11:48:05 -0500

    Add comments to each match arm in hashmap! macro

--- hashmap/src/lib.rs
+++ hashmap/src/lib.rs
@@ -1,6 +1,11 @@
 #[macro_export]
 macro_rules! hashmap {
+    // matches one or more key-value pairs that end with a comma
+    // this rule is necessary in order to match single key-value
+    // pairs that end with a trailing comma
     ($($key:expr => $value:expr,)+) => { $crate::hashmap!($($key => $value),+) };
+    // matches zero or more key-value pairs that are each 
+    // separated by commas
     ($($key:expr => $value:expr),*) => {
         {
             let mut _map = ::std::collections::HashMap::new();
